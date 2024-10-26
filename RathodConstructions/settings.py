7d0de3f46731ec6579cb6fbc2da2aec8d8ed5e8e
NavneetSinhParmar <navneetparmar8952@gmail.com>
2024-10-26 22:33:15 +0530

    added staticfile

--- RathodConstructions/settings.py
+++ RathodConstructions/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'static_collected'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/RathodConstructions/Construction/static/',  # Absolute path to your static files directory
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
